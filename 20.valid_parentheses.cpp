b8ade0c77c0c023c2a94f759cdfa90aaf658da18
Fazlerabbi-Fahad <fazlerabbifahad.54@gmail.com>
2025-08-01 07:15:20 -0700

    Valid Parenthesis

---
+++ 20.valid_parentheses.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (char c : s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    return false;
+                }
+                else
+                {
+                    if (c == ')' && st.top() == '(')
+                    {
+                        st.pop();
+                    }
+                    else if (c == '}' && st.top() == '{')
+                    {
+                        st.pop();
+                    }
+                    else if (c == ']' && st.top() == '[')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+int main()
+{
+    Solution obj;
+    string input;
+
+    cout << "Enter a string of brackets: ";
+    cin >> input;
+
+    if (obj.isValid(input))
+    {
+        cout << "Valid" << endl;
+    }
+    else
+    {
+        cout << "Invalid" << endl;
+    }
+
+    return 0;
+}
